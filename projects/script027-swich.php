9a9ebe4e77b201fc392fcd71227c1ad2ceee46f5
leoallvez <leoallvez@hotmail.com>
2016-01-13 00:53:56 -0200

    v8

--- projects/script027-swich.php
+++ projects/script027-swich.php
@@ -12,12 +12,13 @@
 				$o = isset($_GET["oper"])?$_GET["oper"]:1;
 				switch($o){
 					case 1: $r = $n * 2; break;
-					case 2: $r = $n ^ 3; break;
-					case 3: $r = $n sqrt($n); break;
-
+					case 2: $r = ($n * $n) * $n; break;
+					case 3: $r = sqrt($n); break;
+					#default: echo "Número invalido.";
 				}
-				
+				echo "O resultado da operação solicitada foi $r.";
 			?>
+		</br><a href="script027-swich.html">Voltar</a>
 		</div>
 	</body>
 </html>
