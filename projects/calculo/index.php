ed90990a19c3ede07c007a5bf08aceb8a8a29fc9
leoallvez <leoallvez@hotmail.com>
2016-01-21 02:27:28 -0200

    v14

--- projects/calculo/index.php
+++ projects/calculo/index.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Campus Calc</title>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<link rel="stylesheet" href="_css/style-calc.css">
 		<?php 
 			function notas(){
@@ -15,33 +16,41 @@
 				echo "</datalist>";
 			}
 		?>
-		<style>
-			
-		</style>
+		<script type="text/javascript">
+			function limpa(id){
+				document.getElementById(id).value= " ";
+			}
+
+			function zera(id){
+				var s = document.getElementById(id).value;
+				if(s == ""){
+					document.getElementById(id).value = "0";
+				}
+			}
+		</script>
 	</head>
 	<body>
-		<div>
+		<div id="principal">
 			<form method="get" action="result.php">
 				<h2>Campus Calc<sup><span id="bet"> beta</span></sup></h2>
 				<fieldset>
 					<legend><strong>M1</strong></legend>
 					<label for="nm1">Nota M1:</label></br>
-					<input type="number" name="m1" value="0" id="nm1" list="lista" step="0.1" min="0" max="10"></br>
+					<input type="number" name="m1" value="0" id="nm1" list="lista" step="0.1" min="0" max="10" onFocus="limpa('nm1')" onBlur="zera('nm1')"></br>
 					<?php notas(); ?>
 				</fieldset>
 				<fieldset>
 					<legend><strong>M2</strong></legend>
 					<label for="nnd">Nota Diciplina:</label></br>
-					<input type="number" name="nd" value="0" id="nnd" list="lista" step="0.1" min="0" max="10"></br>
+					<input type="number" name="nd" value="0" id="nnd" list="lista" step="0.1" min="0" max="10" onFocus="limpa('nnd')" onBlur="zera('nnd')"></br>
 					<?php notas(); ?>
 					<label for="nni">Nota Integrada: </label></br>
-					<input type="number" name="ni" value="0" id="nni" list="lista" step="0.1" min="0" max="10"></br>
+					<input type="number" name="ni" value="0" id="nni" list="lista" step="0.1" min="0" max="10" onFocus="limpa('nni')" onBlur="zera('nni')"></br>
 					<?php notas(); ?>
 				</fieldset>
 				<input type="submit" class="bot" value="Calcular">
 				<input type="reset"  class="bot" value="Apagar">
 			</form>
-			
 		</div>
 	</body>
 </html>
